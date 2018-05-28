cf5481301f006201543ac30afdfbf4be42f257af
benhenry1 <benhenry@seas.upenn.edu>
2018-05-28 12:26:20 -0400

    init commit

---
+++ account/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from account.models import Profile
+
+# Register your models here. (Add to Admin CMS)
+admin.site.register(Profile)
---
+++ account/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse
+from account.forms import RegistrationForm
+
+# Create your views here.
+def login(request):
+	if request.user.is_authenticated():
+		return redirect('/account')
+	return render(request, 'account/login.html')
+
+def home(request):
+	return render(request, 'account/home.html')
+
+def register(request):
+	if request.method == "POST":
+		#Handle form submission
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/account')
+		else:
+			return render_to_response('account/register.html', {'form': form})
+	else:
+		form = RegistrationForm()
+		ctxt = {'form': form}
+
+		return render(request, 'account/register.html', ctxt)
